67f949cf81e397c2aeab69beea7bd9b811f8e247
Daniil Boyko <dboyko@novapulsar.com>
2022-01-18 09:48:42 +0300

    testing polymorphism

--- TestingPolymorphism.php
+++ TestingPolymorphism.php
@@ -1,16 +1,105 @@
 <?php
 
-class Main
+//class Main
+//{
+//    public function polymorph (): string {
+//        return "parent" . PHP_EOL;
+//    }
+//}
+//
+//class Second extends Main
+//{
+//    public function polymorph (): string {
+//        $test=parent::polymorph();
+//        return $test . PHP_EOL;
+//    }
+//}
+
+//abstract class Person
+//{
+//    abstract public function greet();
+//}
+//
+//class English extends Person
+//{
+//    public function greet() {
+//        return 'Hello!';
+//    }
+//}
+//
+//class German extends Person
+//{
+//    public function greet() {
+//        return 'Hallo!';
+//    }
+//}
+//
+//class French extends Person
+//{
+//    public function greet() {
+//        return 'Bonjour!';
+//    }
+//}
+//
+//class American extends Person
+//{
+//    public function greet() {
+//        return 'Hi!';
+//    }
+//}
+//
+//$people = [
+//    new English(),
+//    new German(),
+//    new French(),
+//    new American()
+//];
+//
+//function greeting ($people) {
+//    foreach ($people as $person) {
+//        echo $person->greet() . PHP_EOL;
+//    }
+//}
+//
+//greeting($people);
+
+
+interface Greeting
 {
-    public function polymorph (): string {
-        return "parent" . PHP_EOL;
+    public function greet();
+}
+
+class English implements Greeting
+{
+    public function greet() {
+        return 'Hello!';
     }
 }
 
-class Second extends Main
+class German implements Greeting
 {
-    public function polymorph (): string {
-        $test=parent::polymorph();
-        return $test . PHP_EOL;
+    public function greet() {
+        return 'Hallo!';
     }
 }
+
+class French implements Greeting
+{
+    public function greet() {
+        return 'Bonjour!';
+    }
+}
+
+$greeters = [
+    new English(),
+    new German(),
+    new French()
+];
+
+function greeting($greeters) {
+    foreach ($greeters as $greeter) {
+        echo $greeter->greet() . PHP_EOL;
+    }
+}
+
+greeting($greeters);
