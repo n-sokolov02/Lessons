a6aa708930f5add3cb949790636e44681623f40f
Daniil Boyko <dboyko@novapulsar.com>
2022-01-20 06:28:25 +0300

    testing cloning and comparing of objects

--- testingClone.php
+++ testingClone.php
@@ -1,18 +1,37 @@
 <?php
 
+class Address
+{
+    public string $street;
+    public string $city;
+}
+
 class Person
 {
     public string $name;
 
     public function __construct($name) {
         $this->name = $name;
+        $this->address = new Address();
+    }
+
+    public function __clone() {
+        $this->address = clone $this->address;
     }
 }
 
 $bob = new Person('Bob');
-$alex = $bob;
+$bob->address->street = 'North 1st Street';
+$bob->address->city = 'San Jose';
 
+$alex = clone $bob;
 $alex->name = 'Alex';
+$alex->address->street = '1 Apple Park Way';
+$alex->address->city = 'Cupertino';
 
 var_dump($bob);
 var_dump($alex);
+
+function deep_clone($object) {
+    return unserialize(serialize($object));
+}
---
+++ testingComparingObject.php
@@ -0,0 +1,38 @@
+<?php
+
+class Point
+{
+    private int $x;
+    private int $y;
+
+    public function __construct($x, $y) {
+        $this->x = $x;
+        $this->y = $y;
+    }
+}
+
+$p1 = new Point(10,20);
+$p2 = new Point(10,20);
+
+if ($p1 === $p2) {
+    echo 'p1 and p2 are equal.' . PHP_EOL;
+}
+else {
+    echo 'p1 and p2 are not equal.' . PHP_EOL;
+}
+
+$p3 = $p2;
+if ($p2 === $p3) {
+    echo 'p2 and p3 are equal.' . PHP_EOL;
+}
+else {
+    echo 'p2 and p3 are not equal.' . PHP_EOL;
+}
+
+$p4 = new Point(20,10);
+if ($p3 === $p4) {
+    echo 'p3 and p4 are equal.' . PHP_EOL;
+}
+else {
+    echo 'p3 and p4 are not equal.' . PHP_EOL;
+}
