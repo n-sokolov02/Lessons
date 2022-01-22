4388a07ff09a8ccbbdd5910d718da99fba0dd2bf
Daniil Boyko <dboyko@novapulsar.com>
2022-01-22 10:44:02 +0300

    practicing abstract classes and methods

---
+++ testingAllTheSections/practicingAbstractClasses.php
@@ -0,0 +1,40 @@
+<?php
+
+abstract class Model
+{
+    abstract public function chooseModel($data);
+}
+
+class AmericanCar extends Model
+{
+    public function chooseModel($data)
+    {
+        // TODO: Implement chooseModel() method.
+        echo "american ";
+        var_dump($data);
+        echo PHP_EOL;
+    }
+}
+
+class RussianCar extends Model
+{
+    public function chooseModel($data)
+    {
+        // TODO: Implement chooseModel() method.
+        echo "russian ";
+        var_dump($data);
+        echo PHP_EOL;
+    }
+}
+
+class Factory
+{
+    public static function getChoose(): void {
+        $car = new RussianCar();
+        $car_1 = new AmericanCar();
+        $car->chooseModel('abstract_1');
+        $car_1->chooseModel('abstract_2');
+    }
+}
+
+$choose = Factory::getChoose();
