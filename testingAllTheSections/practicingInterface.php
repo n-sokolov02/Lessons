a95bb04d65e11e14bedb40330c63a9f70c8756eb
Daniil Boyko <dboyko@novapulsar.com>
2022-01-22 15:28:28 +0300

    add the second interface for test

--- testingAllTheSections/practicingInterface.php
+++ testingAllTheSections/practicingInterface.php
@@ -6,7 +6,13 @@ interface PracticingInterfaces
     public function isRead($bookName);
 }
 
-class Book implements PracticingInterfaces
+interface TheSecondForPractice extends PracticingInterfaces
+{
+    const READERS_AMOUNT = 10;
+    public function getReaders($bookName);
+}
+
+class Book implements PracticingInterfaces, TheSecondForPractice
 {
     private string $time;
 
@@ -19,16 +25,23 @@ class Book implements PracticingInterfaces
     {
         // TODO: Implement isRead() method.
         if ($bookName == 'Book_1') {
+            echo "Amount of readers: " . self::READERS_AMOUNT . PHP_EOL;
             echo "Yes, the book " . $bookName. " was read at " . $this->time . PHP_EOL;
             echo "Amount of these books: " . self::BOOK_AMOUNT;
         } else {
             echo "No, the book " . $bookName . " was not read" . PHP_EOL;
         }
     }
+
+    public function getReaders($bookName)
+    {
+        // TODO: Implement getReaders() method.
+        $this->isRead($bookName);
+    }
 }
 
 $practiceInterface = new Book('19:28');
 echo "Enter the name of the book: ";
 $chooseBook = readline();
-$practiceInterface->isRead($chooseBook);
+$practiceInterface->getReaders($chooseBook);
 
