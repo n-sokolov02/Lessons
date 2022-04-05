63f4de4aef02cf354e8b57de173fdf3018a8da52
Daniil Boyko <dboyko@novapulsar.com>
2022-04-05 21:55:40 +0300

    updated singleton pattern

--- testingPatterns/generatingPatterns/Singleton/Singleton.php
+++ testingPatterns/generatingPatterns/Singleton/Singleton.php
@@ -2,51 +2,33 @@
 
 class Singleton
 {
-    private static array $instances = [];
-
-    /**
-     * @return Singleton
-     */
+    public static array $instances = [];
 
     public static function getInstance(): Singleton
     {
         $cls = static::class;
+
         if (!isset(self::$instances[$cls]))
         {
             self::$instances[$cls] = new static();
         }
-
         return self::$instances[$cls];
     }
-
-    private function __clone()
-    {
-    }
-
-    private function __construct()
-    {
-    }
-
-    /**
-     * @throws Exception
-     */
-    private function __wakeup(): void
-    {
-        // TODO: Implement __wakeup() method.
-        throw new Exception("Cannot unserialize a singleton.");
-    }
-
 }
 
-function UsersCode()
+function getSingletonInstances()
 {
-    $obj1 = Singleton::getInstance();
-    $obj2 = Singleton::getInstance();
-    if ($obj1 === $obj2) {
-        echo 'Singleton works, both variables contain the same instance' . PHP_EOL;
-    } else {
-        echo 'Singleton failed, variables contain different instances' . PHP_EOL;
+    $object_1 = Singleton::getInstance();
+    $object_2 = Singleton::getInstance();
+
+    if ($object_1 === $object_2)
+    {
+        echo 'Singleton works, both variables contain the same instance';
+    }
+    else
+    {
+        echo 'Singleton failed, variables contain different instances';
     }
 }
 
-UsersCode();
+getSingletonInstances();
--- testingPatterns/generatingPatterns/Singleton/newSingleton/auditSingleton.php
+++ testingPatterns/generatingPatterns/Singleton/newSingleton/auditSingleton.php
@@ -1,1 +1,48 @@
 <?php
+
+class AuditSingleton
+{
+    public static array $instances = [];
+
+    /**
+     * @return AuditSingleton
+     */
+    public static function getAuditInstance(): AuditSingleton
+    {
+        $cls = static::class;
+
+        if (!isset(self::$instances[$cls])) {
+            self::$instances[$cls] = new static();
+        }
+
+        return self::$instances[$cls];
+    }
+
+    private function __construct()
+    {
+    }
+
+    /**
+     * @return mixed
+     * @throws Exception
+     */
+    private function __wakeup()
+    {
+        // TODO: Implement __wakeup() method.
+        throw new Exception('Cannot serialize singleton');
+    }
+}
+
+function auditClientCode()
+{
+    $object_1 = AuditSingleton::getAuditInstance();
+    $object_2 = AuditSingleton::getAuditInstance();
+
+    if ($object_1 === $object_2) {
+        echo 'Singleton works, both variables contain the same instance' . PHP_EOL;
+    } else {
+        echo 'Singleton failed, variables contain different instances' . PHP_EOL;
+    }
+}
+
+auditClientCode();
