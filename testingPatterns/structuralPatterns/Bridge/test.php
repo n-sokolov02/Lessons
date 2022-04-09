be37daa0fe9738a28be0ca4bbe76da2dd72914f7
Daniil Boyko <dboyko@novapulsar.com>
2022-04-09 11:16:47 +0300

    refactor(bridge): consolidate all the code from bridge pattern into the one file

--- testingPatterns/structuralPatterns/Bridge/test.php
+++ testingPatterns/structuralPatterns/Bridge/test.php
@@ -1,1 +1,49 @@
 <?php
+
+interface Formatter
+{
+    public function format(string $text): string;
+}
+
+abstract class Service
+{
+    public Formatter $implementation;
+    public function __construct($implementation)
+    {
+    }
+
+    abstract public function get(): string;
+}
+
+class HTMLFormatter implements Formatter
+{
+    public function format(string $text): string
+    {
+        // TODO: Implement format() method.
+        return sprintf('<p>%s</p>', $text);
+    }
+}
+
+class PlainTextFormatter implements Formatter
+{
+    public function format(string $text): string
+    {
+        // TODO: Implement format() method.
+        return $text;
+    }
+}
+
+class HelloWorldService extends Service
+{
+    public function get(): string
+    {
+        // TODO: Implement get() method.
+        return $this->implementation->format('Hello World');
+    }
+}
+
+$service = new HelloWorldService(new PlainTextFormatter());
+$service->get();
+
+$service_1 = new HelloWorldService(new HTMLFormatter());
+$service_1->get();
