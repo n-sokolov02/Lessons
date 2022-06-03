2d778ba0a8f127fff4c753c50554983dc176e4e1
Daniil Boyko <d.boyko@sxope.com>
2022-06-03 23:26:26 +0300

    updated registry pattern

--- TestingPatterns/StructuralPatterns/Registry/Registry.php
+++ TestingPatterns/StructuralPatterns/Registry/Registry.php
@@ -20,23 +20,8 @@ class Registry
     {
         if (!isset(self::$data[$key])) {
             self::$data[$key] = $value;
-            echo 'INFO: Element was added' . PHP_EOL;
         } else {
-            throw new Exception('Element was already set');
-        }
-    }
-
-    /**
-     * @param $key
-     * @return void
-     * @throws Exception
-     */
-    public static function get($key): void
-    {
-        if (isset(self::$data[$key])) {
-            echo 'INFO: ' . self::$data[$key] . PHP_EOL;
-        } else {
-            throw new Exception('Element is not set');
+            throw new Exception('Element was already built');
         }
     }
 
@@ -49,41 +34,27 @@ class Registry
     {
         if (isset(self::$data[$key])) {
             unset(self::$data[$key]);
-            echo 'INFO: Element was removed' . PHP_EOL;
         } else {
-            throw new Exception('Element was not set or already removed');
+            throw new Exception('Element was already deleted or not added');
+        }
+    }
+
+    /**
+     * @param $key
+     * @return void
+     * @throws Exception
+     */
+    public static function get($key): void
+    {
+        if (isset(self::$data[$key])) {
+            echo self::$data[$key];
+        } else {
+            throw new Exception('Element is not set or = NULL');
         }
     }
 }
 
-function clientCodeWithCatchingExceptions(): void
-{
-    /*
-     try {
-        Registry::add(1, 'PHY_PARTNERS');
-    } catch (Exception $e) {
-    }
-
-    try {
-        Registry::get(1);
-    } catch (Exception $e) {
-    }
-
-    try {
-        Registry::remove(1);
-    } catch (Exception $e) {
-    }
-
-    try {
-        Registry::get(1);
-    } catch (Exception $e) {
-    }
-    */
-
-    Registry::add(1, 'PHY_PARTNERS');
-    Registry::get(1);
-    Registry::remove(1);
-    Registry::get(1);
-}
-
-clientCodeWithCatchingExceptions();
+Registry::add(1, 'PHY_PARTNERS');
+Registry::get(1);
+Registry::remove(1);
+Registry::get(1);
